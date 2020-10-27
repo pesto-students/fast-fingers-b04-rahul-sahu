fd62b5310a640f81207ceee4f50ccf3e9a41bfe3
Rahul <rahulsahu.blog@gmail.com>
2020-10-28 02:49:44 +0530

    Add Component: PlayerInfo, Implement : GameWord

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, Row, Col, Form, Button} from 'react-bootstrap';
-import keyboardIcon from './img/keyboard-icon.png'
+import keyboardIcon from './img/keyboard-icon.png';
+import dict from './dictionary.json';
+
+import PlayerInfo from  './components/PlayerInfo';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       name : '',
-      level : 'easy',
+      level : 1,
+      startPage : true,
+      gamePage : false,
+      inputWord : '',
+      gameWord : '',
     };
 
     this.handleChangeName = this.handleChangeName.bind(this);
     this.handleChangeLevel = this.handleChangeLevel.bind(this);
+  }
+
+  componentDidUpdate(){
+    if(this.state.inputWord === this.state.gameWord){
+      this.setState({
+        inputWord : '',
+        level : (this.state.level + 0.01)
+      });
+      this.getGameWord();
+    }
   }
 
   handleChangeName(event){
@@ -22,41 +39,113 @@ class App extends React.Component {
   }
 
   handleChangeLevel(event){
-    this.setState({level : event.target.value});
+    let level = parseFloat(event.target.value);
+    this.setState({level : level});
   }
-  startGame(){
-    console.log("Start Game");
+
+  handleInputWord = (event) => {
+    this.setState({inputWord : event.target.value});
+  }
+
+  getGameWord = () => {
+    let dictLength = dict.length;
+    let newWord = dict[ Math.floor(Math.random() * dictLength)];
+    if (this.state.level < 1.5) {
+      do {
+        newWord = dict[ Math.floor(Math.random() * dictLength)];
+      } while(newWord.length > 4)
+    }
+    else if (this.state.level >= 1.5 && this.state.level < 2) {
+      do {
+        newWord = dict[ Math.floor(Math.random() * dictLength)];
+      } while(newWord.length < 4 || newWord.length > 8)
+    }
+    else if (this.state.level >=2) {
+      do {
+        newWord = dict[ Math.floor(Math.random() * dictLength)];
+      } while(newWord.length < 9)
+    }
+    this.setState({gameWord : newWord});
+  }
+  startGame = () => {
+    if(this.state.name === ''){
+      alert("Please enter the name")
+    }
+    else{
+      this.setState({
+        startPage : false,
+        gamePage : true
+      });
+      this.getGameWord();
+    }
   }
 
   render(){
     return (
       <div className="App">
         <Container>
-          <div className="start-page">
-            <Row>
-              <img src={ keyboardIcon } alt='keyboard-icon' className="icon-awesome-keyboard my-auto" />
-            </Row>
-            <Row>
-              <h1>fast fingers</h1>
-            </Row>
-            <Row>
-              <p>------------- The ultimate typing game -------------</p>
-            </Row>
-            <Form>
-              <Form.Group controlId="forName">
-                <Form.Control value = {this.state.name} onChange={this.handleChangeName} type="text" placeholder="Enter your name" size="lg"/>
-              </Form.Group>
-              <Form.Group controlId="forLevel">
-                <Form.Control value = {this.state.level} onChange={this.handleChangeLevel} as="select" size="lg">
-                  <option value="easy">Easy</option>
-                  <option value="medium">Medium</option>
-                  <option value="hard">Hard</option>
-                </Form.Control>
-              </Form.Group>
-            </Form>
-            <Button variant="danger" onClick={this.startGame}>Start Game</Button>
-          </div>
-        </Container>
+          {
+            this.state.startPage &&
+            <div className="start-page">
+              <Row>
+                <img src={ keyboardIcon } alt='keyboard-icon' className="icon-awesome-keyboard my-auto" />
+              </Row>
+              <Row>
+                <h1>fast fingers</h1>
+              </Row>
+              <Row>
+                <p>------------- The ultimate typing game -------------</p>
+              </Row>
+              <Form>
+                <Form.Group controlId="forName">
+                  <Form.Control value = {this.state.name} onChange={this.handleChangeName} type="text" placeholder="Enter your name" size="lg"/>
+                </Form.Group>
+                <Form.Group controlId="forLevel">
+                  <Form.Control value = {this.state.level} onChange={this.handleChangeLevel} as="select" size="lg">
+                    <option value={1}>Easy</option>
+                    <option value={1.5}>Medium</option>
+                    <option value={2}>Hard</option>
+                  </Form.Control>
+                </Form.Group>
+              </Form>
+              <Button variant="danger" onClick={this.startGame}>Start Game</Button>
+            </div>
+        
+          }
+          {
+            this.state.gamePage &&
+            <div className="game-page">
+              <Row>
+                <Col>
+                  <PlayerInfo
+                    name = {this.state.name}
+                    level = {this.state.level}
+                  />
+                </Col>
+                <Col>
+                  <div>Fast Fingers</div>
+                  <div>Score : 00:30</div>
+                </Col>
+              </Row>
+              <div>
+                <div className="score-board">
+                  Score Board
+                </div>
+                <div className="game-box">
+                  <div className="game-timer">
+
+                  </div>
+                  <div className="game-word">
+                    {this.state.gameWord}
+                  </div>
+                  <div className="game-input-wrapper">
+                    <input type="text" value={this.state.inputWord} onChangeCapture={this.handleInputWord}></input> 
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+          </Container>
       </div>
     );
   }
---
+++ src/components/PlayerInfo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Row, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './PlayerInfo.css';
+
+import iconPerson from './../img/icon-person.png';
+import iconGamepad from './../img/icon-gamepad.png';
+
+
+class PlayerInfo extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            level : ''
+        }
+    }
+
+    render(){
+        let level = '';
+        if(this.props.level < 1.5){
+            level = 'EASY';
+        }
+        else if (this.props.level >= 1.5 && this.props.level < 2){
+            level = 'MEDIUM';
+        }
+        else if (this.props.level <= 2){
+            level = 'HARD';
+        }
+        return (
+            <div className="player-info">
+                <div className="name-wrapper">
+                    <img src={iconPerson} alt='icon-player' className='icon-person'/>
+                    <div className="player-info-name"> Player : {this.props.name}</div>
+                </div>
+                <div className="name-wrapper">
+                    <img src={iconGamepad} alt='icon-gamepad' className='icon-person'/>
+                    <div className="player-info-name"> Level : {level}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PlayerInfo;
